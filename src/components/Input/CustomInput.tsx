bdd26cac8634dca4e9fc48610edf9e61bdbd8247
witekmikolajczak <witek.mikolajczak@e.askwho.net>
2022-11-29 20:52:02 +0100

    create input component

---
+++ src/components/Input/CustomInput.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import styles from './CustomInput.module.scss';
+
+interface InputProps {
+  label: boolean;
+  labelText?: string;
+  className?: string;
+  placeholder: string;
+  name: string;
+  value: string | number | undefined;
+  fnHandleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+export const CustomInput = ({
+  label,
+  labelText,
+  className,
+  placeholder,
+  name,
+  value,
+  fnHandleChange,
+}: InputProps) => {
+  return (
+    <div className={`${styles.wrapper} ${className}`}>
+      {label && (
+        <label className={styles.label} htmlFor={name}>
+          {labelText}
+        </label>
+      )}
+      <input
+        className={styles.input}
+        value={value}
+        name={name}
+        placeholder={placeholder}
+        onChange={(event) => fnHandleChange(event)}
+      />
+    </div>
+  );
+};
