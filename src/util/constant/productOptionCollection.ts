ce5940b77c1bedc1a6cd8eff4b147923325bc976
witekmikolajczak <witek.mikolajczak@e.askwho.net>
2022-11-29 20:52:46 +0100

    new utils

---
+++ src/util/constant/productOptionCollection.ts
@@ -0,0 +1,8 @@
+export const productUnitCollection: ProductUnitsInterface[] = [
+  {
+    type: 'weight',
+    text: 'Waga',
+    units: [{ unit: 'kg' }, { unit: 'g' }, { unit: 'dag' }],
+  },
+  { type: 'count', text: 'Ilość', units: [{ unit: 'szt' }] },
+];
