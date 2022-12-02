e440e9ab48073918a74948ce418ba941134df91e
witekmikolajczak <witek.mikolajczak@e.askwho.net>
2022-12-02 20:42:07 +0100

    add logout route

--- src/util/constant/links.tsx
+++ src/util/constant/links.tsx
@@ -1,39 +1,48 @@
 import { GrFormAdd } from 'react-icons/gr';
+import { CiLogout } from 'react-icons/ci';
 import {
   AiFillEdit,
   AiOutlineDelete,
   AiOutlineOrderedList,
   AiOutlineHome,
 } from 'react-icons/ai';
+
 export const SIDEBAR_LINKS_COLLECTION = [
   {
     id: '0',
+    name: 'Strona główna',
+    path: 'dashboard',
+    icon: <AiOutlineHome size={20} />,
+  },
+  {
+    id: '1',
     name: 'Dodaj produkt',
     path: 'add-product',
     icon: <GrFormAdd size={20} />,
   },
   {
-    id: '1',
+    id: '2',
     name: 'Edytuj produkt',
     path: 'edit-product',
     icon: <AiFillEdit size={20} />,
   },
   {
-    id: '2',
+    id: '3',
     name: 'Usuń produkt',
     path: 'delete-product',
     icon: <AiOutlineDelete size={20} />,
   },
   {
-    id: '2',
+    id: '4',
     name: 'Lista przepisów',
     path: 'recipt-list',
     icon: <AiOutlineOrderedList size={20} />,
   },
+
   {
-    id: '3',
-    name: 'Strona główna',
-    path: 'dashboard',
-    icon: <AiOutlineHome size={20} />,
+    id: '5',
+    name: 'Wyloguj',
+    path: 'auth',
+    icon: <CiLogout size={20} />,
   },
 ];
